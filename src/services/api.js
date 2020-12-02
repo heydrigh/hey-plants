9ed417e01315b12f8be8c98585b6ce96708a063e
heydrigh <heydrigh@gmail.com>
2020-12-02 14:49:36 -0300

    adding new address

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: ` http://${process.env.REACT_APP_API_ADD}`,
+  baseURL: process.env.REACT_APP_API_ADD
 });
 
 export default api;
